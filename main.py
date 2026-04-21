a7624526a9fc1f9fcb7f4fcbfe08519f7285f112
Sayan Ganguly <sayanganguly718@gmail.com>
2026-04-21 22:21:50 +0530

    Render deploy ready

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from src.routes.chat_router import router as chat_router
 
 app = FastAPI()
 
-
-
 app.include_router(chat_router)
 
+@app.get("/")
+def home():
+    return {"message": "DishaAI Backend Running"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
-
+    uvicorn.run(app, host="0.0.0.0", port=8000)
